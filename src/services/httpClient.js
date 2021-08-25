ed20f3e4c8dc1b136029feb6781f3302e2064de0
Serhat KAYA <serhatkaya.abarth@gmail.com>
2021-08-25 19:20:45 +0300

    httpclient updated

--- src/services/httpClient.js
+++ src/services/httpClient.js
@@ -1,8 +1,30 @@
 import axios from 'axios';
-export default axios.create({
+
+const instance = axios.create({
     baseURL: 'http://localhost:3000/api/v1',
     timeout: 5000,
-    headers: {
-        'Content-Type': 'application/json',
-    },
 });
+
+instance.interceptors.request.use(
+    function(config) {
+        // Do something before request is sent
+        console.log('request send');
+        return config;
+    },
+    function(error) {
+        // Do something with request error
+        return Promise.reject(error);
+    }
+);
+
+instance.interceptors.response.use(
+    function(response) {
+        console.log('repsonse get');
+        return response;
+    },
+    function(error) {
+        return Promise.reject(error);
+    }
+);
+
+export default instance;
